c21b4a97ef07b1af81c278389c289b5cc7cb8a01
bondarenko-nvladi <natalibondarenko1992@yandex.ru>
2026-07-10 22:10:33 +0300

    Повторила код из урока; Написала автотест в котором будет переход к восстановлению по телефону; Написала автотест в котором будет переход к восстановлению по почте

--- src/main/java/core/pages/LoginPage.java
+++ src/main/java/core/pages/LoginPage.java
@@ -1,11 +1,10 @@
-package pages;
+package core.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
 
 public class LoginPage {
     private SelenideElement usernameField = $("[name='login-phone-email']");
@@ -14,6 +13,10 @@ public class LoginPage {
     private SelenideElement forgotPasswordLink = $("[name='forgot-password-link']");
     private SelenideElement registrationButton = $("[name='hero-register-btn']");
 
+    private SelenideElement phoneTitle = $("[name='phone-title']");
+    private SelenideElement emailTitle = $("[name='email-title']");
+
+
     //Локаторы для кнопок соцсетей
 //    private SelenideElement vkButton = $("[data-l='t,vkc']");
 //    private SelenideElement googleButton = $("[data-l='t,google']");
@@ -21,6 +24,9 @@ public class LoginPage {
 
     //Локатор для элемента с сообщением об ошибка входа
     private SelenideElement errorMessage = $("[name='login-error']");
+
+    //локатор для перехода к восстановлению
+    private SelenideElement goToRecoveryButton = $("[name='lockout-recover-btn']");
 
     {
         verifyPageElements();
@@ -45,6 +51,18 @@ public class LoginPage {
         return errorMessage.shouldBe(visible).exists();
     }
 
+    @Step("Проверем видимость титульного названия 'Указать телефон' на странице поиска по телефону")
+    public boolean isMessageVisiblePhonePage() {
+        phoneTitle.shouldBe(visible);
+        return phoneTitle.shouldBe(visible).exists();
+    }
+
+    @Step("Проверем видимость титульного названия 'Почта' на странице поиска по почте")
+    public boolean isMessageVisibleEmailPage() {
+        emailTitle.shouldBe(visible);
+        return emailTitle.shouldBe(visible).exists();
+    }
+
     @Step("Получаем текст сообщения об ошибке входа")
     public String getErrorMessageText() {
         return errorMessage.shouldBe(visible).getText();
@@ -59,6 +77,22 @@ public class LoginPage {
         loginButton.shouldBe(visible).click();
     }
 
+    @Step ("Входим на сайт с логином {username} and {password}")
+    public void setPassword(String password) {
+        passwordField.shouldBe(visible).click();
+        passwordField.shouldBe(visible).setValue(password);
+    }
+
+    @Step("Нажимаем на кнопку Войти")
+    public void clickLogin() {
+        loginButton.shouldBe(visible).click();
+    }
+
+    @Step("Нажимаем Восстановить профиль")
+    public void goToRecovery() {
+        goToRecoveryButton.shouldBe(visible).click();
+    }
+
     @Step("Переходим на страницу восстановления пароля")
     public void openForgotPasswordPage() {
         forgotPasswordLink.shouldBe(visible).click();
@@ -68,6 +102,8 @@ public class LoginPage {
     public void openRegistrationPage() {
         registrationButton.shouldBe(visible).click();
     }
+
+
 
 //    // Методы для перехода на страницы авторизации через соцсети
 //    @Step("Входим на сайт через Вконтакте")
